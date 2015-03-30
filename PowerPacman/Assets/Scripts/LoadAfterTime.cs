c8d8bf7d461704049c758561a4ad0084c50f01b4
rawilder <rawilder0506@gmail.com>
2015-03-30 12:25:05 -0500

    fixed nullreferenceexception and changed a javascript script to a c# script

---
+++ PowerPacman/Assets/Scripts/LoadAfterTime.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadAfterTime : MonoBehaviour {
+
+    public int Delay = 5;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        yield return new WaitForSeconds(Delay);
+        Application.LoadLevel(2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
