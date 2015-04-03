de0aff9e5449556f9bd678566aa2f9620fd7844a
GarrisonK <gkeller572@gmail.com>
2015-04-03 00:10:46 -0500

    Started admin console, added drop down

---
+++ PowerPacman/Assets/Scripts/DropdownScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DropdownScript : MonoBehaviour {
+
+	public Transform dropdown;
+
+	public GameObject DropDownPrefab;
+
+	void Start(){
+
+		for (int i = 0; i < 5; i++) {
+			GameObject button = (GameObject) Instantiate(DropDownPrefab);
+			button.GetComponentInChildren<Text>().text = "Button " + i;
+			int index = i;
+			button.GetComponent<Button>().onClick.AddListener(
+				() => {Debug.Log ("Button " + index + " clicked");}
+			);
+			button.transform.SetParent(dropdown,false);
+		}
+
+	}
+
+
+	/*
+	public void toggle(){
+		if (dropdown.activeSelf == true) {
+			dropdown.SetActive (false);
+		} else {
+			dropdown.SetActive(true);
+		}
+	}
+	*/
+
+
+}
