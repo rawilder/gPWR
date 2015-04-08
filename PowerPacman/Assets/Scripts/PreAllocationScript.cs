42c0528c31f5fa3c4134c8aa682b8b4758f0f8d0
GarrisonK <gkeller572@gmail.com>
2015-04-07 22:18:44 -0500

    Removed allocation stage for control scenarios

---
+++ PowerPacman/Assets/Scripts/PreAllocationScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PreAllocationScript : MonoBehaviour {
+
+	Text messageText;
+
+	void Start(){
+
+		messageText = GameObject.Find ("Message").GetComponent<Text> ();
+
+		if (DataScript.scenario.control) {
+
+			messageText.text = "This is some placeholder text letting the user know that the game will be beginning soon (no allocation)";
+
+		} else {
+			if(DataScript.scenario.playerHasHighPower){
+				messageText.text = "This is some placeholder text telling the user that they will be allocating power ups to themselves and the other player";
+			}
+			else{
+				messageText.text = "This is some placeholder text telling the user that they will have powerups allocated to them by the other player";
+			}
+		}
+
+	}
+
+	public void advanceStage(){
+
+		if (DataScript.scenario.control) {
+			//head right into the game
+			Application.LoadLevel (2);
+		} else {
+			//advance to allocation stage
+			Application.LoadLevel(7);
+		}
+
+	}
+
+}
