d48554e79163110b73307f731709beec8f1e843e
GarrisonK <gkeller572@gmail.com>
2015-04-11 22:59:32 -0500

    Added part of allocation screen

--- PowerPacman/Assets/Scripts/PowerupAllocationScript.cs
+++ PowerPacman/Assets/Scripts/PowerupAllocationScript.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PowerupAllocationScript : MonoBehaviour {
 
 	Text messageText;
 	public Text Title;
+	public Transform sliderPanel;
+	public GameObject allocationSliderPrefab;
+
+	IList<GameObject> sliders = new List<GameObject>();
 
 	void Start () {
 	
@@ -56,6 +61,23 @@ public class PowerupAllocationScript : MonoBehaviour {
 		if (DataScript.scenario.hpStealsTurnsAvailable) {
 			messageText.text += " High power can steal turns,";
 		}
+
+
+		//create the sliders
+		createSliders ();
+
+
+
+		if (!DataScript.scenario.playerHasHighPower){
+			foreach (var slider in sliders) {
+				slider.GetComponentInChildren<Slider>().enabled = false;
+				Graphic[] g = slider.GetComponentInChildren<Slider>().GetComponentsInChildren<Graphic>();
+				for(int i = 0; i < g.Length; i++){
+					g[i].CrossFadeAlpha(.2f,.2f,true);
+				}
+			}
+		}
+
 
 		if (!DataScript.scenario.playerHasHighPower) {
 			if(!DataScript.scenario.AiAllocationIsRandom){
@@ -139,6 +161,156 @@ public class PowerupAllocationScript : MonoBehaviour {
 
 
 			}
+		}
+	}
+
+	void createSliders(){
+
+		if (DataScript.scenario.pSpeedIncreaseAvailable) {
+			GameObject newSlider = (GameObject)Instantiate (allocationSliderPrefab);
+			newSlider.GetComponentInChildren<Text> ().text = "Player Speed Increase";
+			newSlider.GetComponentInChildren<Slider> ().onValueChanged.AddListener ((float value) => {
+				
+				if (DataScript.scenario.playerHasHighPower) {
+					int v = updateSliderValue (newSlider.GetComponentInChildren<Slider> ());
+					DataScript.alloc.PlayerSpeed = v;
+				}
+			}
+			);
+			newSlider.transform.SetParent (sliderPanel, false);
+			sliders.Add (newSlider);
+		}
+		
+		if (DataScript.scenario.gSpeedDecreaseAvailable) {
+			GameObject newSlider = (GameObject)Instantiate (allocationSliderPrefab);
+			newSlider.GetComponentInChildren<Text> ().text = "Enemy Speed Decrease";
+			newSlider.GetComponentInChildren<Slider> ().onValueChanged.AddListener ((float value) => {
+				
+				if (DataScript.scenario.playerHasHighPower) {
+					int v = updateSliderValue (newSlider.GetComponentInChildren<Slider> ());
+					DataScript.alloc.GhostSpeed = v;
+				}
+			}
+			);
+			newSlider.transform.SetParent (sliderPanel, false);
+			sliders.Add (newSlider);
+		}
+		
+		if (DataScript.scenario.fRespawnAvailable) {
+			GameObject newSlider = (GameObject)Instantiate (allocationSliderPrefab);
+			newSlider.GetComponentInChildren<Text> ().text = "Fruit Respawn Increase";
+			newSlider.GetComponentInChildren<Slider> ().onValueChanged.AddListener ((float value) => {
+				
+				if (DataScript.scenario.playerHasHighPower) {
+					int v = updateSliderValue (newSlider.GetComponentInChildren<Slider> ());
+					DataScript.alloc.FruitRespawn = v;
+				}
+			}
+			);
+			newSlider.transform.SetParent (sliderPanel, false);
+			sliders.Add (newSlider);
+		}
+
+		if (DataScript.scenario.longerPowerModeAvailable) {
+			GameObject newSlider = (GameObject)Instantiate (allocationSliderPrefab);
+			newSlider.GetComponentInChildren<Text> ().text = "Longer Power Mode";
+			newSlider.GetComponentInChildren<Slider> ().onValueChanged.AddListener ((float value) => {
+				
+				if (DataScript.scenario.playerHasHighPower) {
+					int v = updateSliderValue (newSlider.GetComponentInChildren<Slider> ());
+					DataScript.alloc.LongerPowerMode = v;
+				}
+			}
+			);
+			newSlider.transform.SetParent (sliderPanel, false);
+			sliders.Add (newSlider);
+		}
+
+		if (DataScript.scenario.longerPowerModeAvailable) {
+			GameObject newSlider = (GameObject)Instantiate (allocationSliderPrefab);
+			newSlider.GetComponentInChildren<Text> ().text = "Longer Power Mode";
+			newSlider.GetComponentInChildren<Slider> ().onValueChanged.AddListener ((float value) => {
+				
+				if (DataScript.scenario.playerHasHighPower) {
+					int v = updateSliderValue (newSlider.GetComponentInChildren<Slider> ());
+					DataScript.alloc.LongerPowerMode = v;
+				}
+			}
+			);
+			newSlider.transform.SetParent (sliderPanel, false);
+			sliders.Add (newSlider);
+		}
+		if (DataScript.scenario.powerballRespawnAvailable) {
+			GameObject newSlider = (GameObject)Instantiate (allocationSliderPrefab);
+			newSlider.GetComponentInChildren<Text> ().text = "Power Balls Respawn";
+			newSlider.GetComponentInChildren<Slider> ().onValueChanged.AddListener ((float value) => {
+				
+				if (DataScript.scenario.playerHasHighPower) {
+					int v = updateSliderValue (newSlider.GetComponentInChildren<Slider> ());
+					DataScript.alloc.PowerBallRespawn = v;
+				}
+			}
+			);
+			newSlider.transform.SetParent (sliderPanel, false);
+			sliders.Add (newSlider);
+		}
+
+		if (DataScript.scenario.gRespawnAvailable) {
+			GameObject newSlider = (GameObject)Instantiate (allocationSliderPrefab);
+			newSlider.GetComponentInChildren<Text> ().text = "Enemy Slower Respawn";
+			newSlider.GetComponentInChildren<Slider> ().onValueChanged.AddListener ((float value) => {
+				
+				if (DataScript.scenario.playerHasHighPower) {
+					int v = updateSliderValue (newSlider.GetComponentInChildren<Slider> ());
+					DataScript.alloc.GhostRespawn = v;
+				}
+			}
+			);
+			newSlider.transform.SetParent (sliderPanel, false);
+			sliders.Add (newSlider);
+		}
+
+		if (DataScript.scenario.gDumbAvailale) {
+			GameObject newSlider = (GameObject)Instantiate (allocationSliderPrefab);
+			newSlider.GetComponentInChildren<Text> ().text = "\"Dumb\" Enemies";
+			newSlider.GetComponentInChildren<Slider> ().onValueChanged.AddListener ((float value) => {
+				
+				if (DataScript.scenario.playerHasHighPower) {
+					int v = updateSliderValue (newSlider.GetComponentInChildren<Slider> ());
+					DataScript.alloc.DumbGhosts = v;
+				}
+			}
+			);
+			newSlider.transform.SetParent (sliderPanel, false);
+			sliders.Add (newSlider);
+		}
+
+		if (DataScript.scenario.gFewerAvailable) {
+			GameObject newSlider = (GameObject)Instantiate (allocationSliderPrefab);
+			newSlider.GetComponentInChildren<Text> ().text = "Fewer Enemies";
+			newSlider.GetComponentInChildren<Slider> ().onValueChanged.AddListener ((float value) => {
+				
+				if (DataScript.scenario.playerHasHighPower) {
+					int v = updateSliderValue (newSlider.GetComponentInChildren<Slider> ());
+					DataScript.alloc.FewerGhosts = v;
+				}
+			}
+			);
+			newSlider.transform.SetParent (sliderPanel, false);
+			sliders.Add (newSlider);
+		}
+
+
+	}
+
+	int updateSliderValue(Slider s){
+		if(s.value <= .5){
+			s.value = 0.0f;
+			return 0;
+		}
+		else{
+			s.value = 1.0f;
+			return 1;
 		}
 	}
 }
