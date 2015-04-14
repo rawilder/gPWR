f321ed567e004ece1236a69c3adb41c441f7cec0
GarrisonK <gkeller572@gmail.com>
2015-04-13 23:48:58 -0500

    Added dynamic power up descriptions

--- PowerPacman/Assets/Scripts/Tutorial3Script.cs
+++ PowerPacman/Assets/Scripts/Tutorial3Script.cs
@@ -7,11 +7,58 @@ public class Tutorial3Script : MonoBehaviour {
 	public Text Tut3Title;
 	public Text Tut3Body;
 
+	public Transform descriptionPanel;
+	public GameObject descriptionPrefab;
+
 	// Use this for initialization
 	void Start () {
 	
 		Tut3Title.text = DataScript.tutText.BonusesScreenTitle;
 		Tut3Body.text = DataScript.tutText.BonusesScreenBody;
+
+
+		//populate the power up description panel with all available power ups
+		if (DataScript.scenario.pSpeedIncreaseAvailable) {
+			addNewDescription("Player Speed Increase","Increases the player's speed");
+		}
+		if (DataScript.scenario.gSpeedDecreaseAvailable) {
+			addNewDescription("Enemy Speed Decrease","Decreases the enemy movement speed");
+		}
+		if (DataScript.scenario.fRespawnAvailable) {
+			addNewDescription("Fruit Respawn Increase","Increases the rate at which fruits appear on the map");
+		}
+		if (DataScript.scenario.longerPowerModeAvailable) {
+			addNewDescription("Longer Super Mode", "Increases the length of time that enemies can be eaten");
+		}
+		if (DataScript.scenario.powerballRespawnAvailable) {
+			addNewDescription("Super Balls Respawn","Super balls will reappear after a short time");
+		}
+		if (DataScript.scenario.gRespawnAvailable) {
+			addNewDescription("Enemy Slower Respawn","Enemies take longer to respawn after being eaten");
+		}
+		if (DataScript.scenario.gDumbAvailale) {
+			addNewDescription("\"Dumb\" Enemies","Enemies chase less effectively");
+		}
+		if (DataScript.scenario.gFewerAvailable) {
+			addNewDescription("Fewer Enemies","Only 3 ghosts chase the player instead of 4");
+		}
+
+	}
+
+
+	void addNewDescription(string name, string desc){
+
+		GameObject newDesc = (GameObject)Instantiate (descriptionPrefab);
+		var texts = newDesc.GetComponentsInChildren<Text>();
+		foreach(var textbox in texts){
+			if(textbox.name == "NameBox"){
+				textbox.text = name;
+			}
+			else if(textbox.name == "DescriptionBox"){
+				textbox.text = desc;
+			}
+		}
+		newDesc.transform.SetParent (descriptionPanel, false);
 
 	}
 }
